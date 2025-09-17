4a02a72c672a58e27afb7409e01d793fcf8d3c96
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-09-17 15:58:42 -0500

    feat: add UserController test codes

---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,5 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'hyunlee.289@gmail.com', 'hyun', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'hyunlee.290@gmail.com', 'hyun', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
