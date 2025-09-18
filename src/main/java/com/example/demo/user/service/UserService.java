6a3757d89a3e14ffa6b330af6efb299537d878ca
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-09-18 10:51:26 -0500

    refactor: change package structure.

---
+++ src/main/java/com/example/demo/user/service/UserService.java
@@ -0,0 +1,72 @@
+package com.example.demo.user.service;
+
+import com.example.demo.common.domain.exception.CertificationCodeNotMatchedException;
+import com.example.demo.common.domain.exception.ResourceNotFoundException;
+import com.example.demo.user.domain.UserCreateDto;
+import com.example.demo.user.domain.UserStatus;
+import com.example.demo.user.domain.UserUpdateDto;
+import com.example.demo.user.infrastructure.UserEntity;
+import com.example.demo.user.service.port.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Clock;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final CertificationService certificationService;
+
+    public UserEntity getByEmail(String email) {
+        return userRepository.findByEmailAndStatus(email, UserStatus.ACTIVE)
+            .orElseThrow(() -> new ResourceNotFoundException("Users", email));
+    }
+
+    public UserEntity getById(long id) {
+        return userRepository.findByIdAndStatus(id, UserStatus.ACTIVE)
+            .orElseThrow(() -> new ResourceNotFoundException("Users", id));
+    }
+
+    @Transactional
+    public UserEntity create(UserCreateDto userCreateDto) {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setEmail(userCreateDto.getEmail());
+        userEntity.setNickname(userCreateDto.getNickname());
+        userEntity.setAddress(userCreateDto.getAddress());
+        userEntity.setStatus(UserStatus.PENDING);
+        userEntity.setCertificationCode(UUID.randomUUID().toString());
+        userEntity = userRepository.save(userEntity);
+
+        certificationService.send(userEntity.getEmail(), userEntity.getId(), userEntity.getCertificationCode());
+
+        return userEntity;
+    }
+
+    @Transactional
+    public UserEntity update(long id, UserUpdateDto userUpdateDto) {
+        UserEntity userEntity = getById(id);
+        userEntity.setNickname(userUpdateDto.getNickname());
+        userEntity.setAddress(userUpdateDto.getAddress());
+        userEntity = userRepository.save(userEntity);
+        return userEntity;
+    }
+
+    @Transactional
+    public void login(long id) {
+        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Users", id));
+        userEntity.setLastLoginAt(Clock.systemUTC().millis());
+    }
+
+    @Transactional
+    public void verifyEmail(long id, String certificationCode) {
+        UserEntity userEntity = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Users", id));
+        if (!certificationCode.equals(userEntity.getCertificationCode())) {
+            throw new CertificationCodeNotMatchedException();
+        }
+        userEntity.setStatus(UserStatus.ACTIVE);
+    }
+}
---
+++ src/test/java/com/example/demo/user/infrastructure/UserJpaRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.example.demo.user.infrastructure;
+
+import com.example.demo.user.domain.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(showSql = true)
+@TestPropertySource("classpath:test-application.properties")
+@Sql("/sql/user-repository-test-data.sql")
+class UserJpaRepositoryTest {
+
+    @Autowired
+    private UserJpaRepository userJpaRepository;
+
+    @Test
+    void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        //given
+        //when
+        Optional<UserEntity> result = userJpaRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        //then
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        //given
+        //when
+        Optional<UserEntity> result = userJpaRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+        //then
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        //given
+        //when
+        Optional<UserEntity> result = userJpaRepository.findByEmailAndStatus("hyunlee.289@gmail.com", UserStatus.ACTIVE);
+
+        //then
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        //given
+        //when
+        Optional<UserEntity> result = userJpaRepository.findByEmailAndStatus("hyunlee.289@gmail.coma", UserStatus.ACTIVE);
+
+        //then
+        assertThat(result).isEmpty();
+    }
+
+}
