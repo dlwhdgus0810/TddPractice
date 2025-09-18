c8b36a59930918a22792e6ef382963c585271fe9
JHL <43563245+dlwhdgus0810@users.noreply.github.com>
2025-09-18 16:50:57 -0500

    feat: add getById throwing ResourceNotFoundException if null.

--- src/main/java/com/example/demo/user/infrastructure/UserRepositoryImpl.java
+++ src/main/java/com/example/demo/user/infrastructure/UserRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.example.demo.user.infrastructure;
 
+import com.example.demo.common.domain.exception.ResourceNotFoundException;
+import com.example.demo.user.domain.User;
 import com.example.demo.user.domain.UserStatus;
 import com.example.demo.user.service.port.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,22 +16,29 @@ public class UserRepositoryImpl implements UserRepository {
     private final UserJpaRepository userJpaRepository;
 
     @Override
-    public Optional<com.example.demo.user.domain.User> findByIdAndStatus(long id, UserStatus userStatus) {
-        return userJpaRepository.findByIdAndStatus(id, userStatus).map(UserEntity::toModel);
-    }
-
-    @Override
-    public Optional<com.example.demo.user.domain.User> findByEmailAndStatus(String email, UserStatus userStatus) {
-        return userJpaRepository.findByEmailAndStatus(email, userStatus).map(UserEntity::toModel);
-    }
-
-    @Override
-    public com.example.demo.user.domain.User save(com.example.demo.user.domain.User user) {
+    public User save(User user) {
         return userJpaRepository.save(UserEntity.fromModel(user)).toModel();
     }
 
     @Override
-    public Optional<com.example.demo.user.domain.User> findById(long id) {
+    public Optional<User> findById(long id) {
         return userJpaRepository.findById(id).map(UserEntity::toModel);
     }
+
+    @Override
+    public User getById(long id) {
+        return findById(id).orElseThrow(() -> new ResourceNotFoundException("Users", id));
+    }
+
+    @Override
+    public Optional<User> findByIdAndStatus(long id, UserStatus userStatus) {
+        return userJpaRepository.findByIdAndStatus(id, userStatus).map(UserEntity::toModel);
+    }
+
+    @Override
+    public Optional<User> findByEmailAndStatus(String email, UserStatus userStatus) {
+        return userJpaRepository.findByEmailAndStatus(email, userStatus).map(UserEntity::toModel);
+    }
+
+
 }
